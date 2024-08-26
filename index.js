127757ba7e248633768e73aff9de79812c1ad154
syqlon <muhammadsaqlainnoor31@gmail.com>
2024-08-26 15:35:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var isfeepaid = "yes";
+
+var isadmissiongranted = "no";
+
+alert(isfeepaid);
+
+document.write(isadmissiongranted); 
+
+var nationlity = "Pakistani";
+ alert(nationlity);
+
+var age = "18";
+document.write(age); 
