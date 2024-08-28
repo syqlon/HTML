14234b8b73ad62a4d4476d0ca5feb0c3995b50cf
syqlon <muhammadsaqlainnoor31@gmail.com>
2024-08-28 15:25:53 +0500

    Add files via upload

---
+++ no5.js
@@ -0,0 +1,58 @@
+
+
+var age = "I " + "am " + 18 + " years " + " old";
+
+alert(age);
+
+var birthAge = "My " + "birth " + "year " + "is " + 2006 ;
+
+document.write(birthAge);
+
+var a = 5; var b = 8; 
+var c = "sum " + "of " + "a " + "and " + "b " + "is " + a+b ;
+var e = "product " + "of " + "a " + "and " + "b " + "is " + a*b ;
+var f = "modulus " + "of " + "a " + "and " + "b " + "is " + a/b ;
+
+console.log(c);
+console.log(e);
+console.log(f);
+
+
+var name = "Saqlain";
+var city = "Islamabad";
+var passport = "Pakistan";
+
+alert(name);
+alert(city);
+alert(passport);
+
+
+var emailAddress = "My " + "email " + "is " + "example@gmail.com"
+ alert(emailAddress)
+
+var m = 7; var n = 5;
+--m;
+alert(m);
+var l = --m - --n;
+alert(l);
+l = l + ++n;
+alert(l);
+l = l + n-- ;
+alert(l);
+
+var dollar1 = 155;
+var rupee = 24*dollar1;
+alert(rupee);
+
+var riyal1 = 41;
+var rupee = 10*riyal1;
+alert(rupee)
+
+tempFaren = 45;
+var tempCelsius = (tempFaren - 32)*5/9;
+alert(tempCelsius);
+
+tempCelsius = 28
+var tempFaren = (tempCelsius*9/5) + 32;
+alert(tempFaren);
+
