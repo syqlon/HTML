03ffa0e70f40f8d2462d0243360f1d6434b25d0a
syqlon <muhammadsaqlainnoor31@gmail.com>
2024-08-28 16:42:37 +0500

    Add files via upload

---
+++ prac1.js
@@ -0,0 +1,53 @@
+var name = "John";
+console.log(name);
+
+name = 45;
+console.log(name);
+
+name = true;
+console.log(name);
+
+
+
+var age = 67;
+var city = "Tokyo";
+var isfeepaid = true;
+
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof isfeepaid);
+
+
+var a = 7;
+var b = 8;
+var c = 0;
+c = b-a;
+ alert(c);
+
+var d = 89
+var e = 65
+var f = 0
+f = d-e
+alert(f) 
+
+var passport = "Pakistan";
+console.log(passport);
+//comments are just to make codes understandable
+
+// some names cannot be used as variable names
+
+
+var a = (9-8)*5+41/7;
+console.log(a); //BODMAS rule
+
+var z = 90;
+z-= 6;
+console.log(z);
+
+var w = 4;
+var x = 7;
+w = --x;
+var y = ++w - --x;
+document.write(y);
+
+
